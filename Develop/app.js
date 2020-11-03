d5291c049818bc3f6ba503677888ce69efb1f8ca
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2020-11-03 00:45:05 -0500

    added initial cli app and tests

---
+++ Develop/app.js
@@ -0,0 +1,99 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const {
+    listenerCount
+} = require("process");
+
+start()
+const employees = [];
+
+function start() {
+    // createTeamHtml()
+    CreateNewEmployee();
+}
+
+async function CreateNewEmployee() {
+    let employee = '';
+    let role = '';
+    await inquirer.prompt([{
+            type: "input",
+            message: "What is the member's name?",
+            name: "nameResponse"
+        },
+        {
+            type: 'input',
+            message: "What is the member's id?",
+            name: 'idResponse'
+        }, {
+            type: 'input',
+            message: "What is the member's email?",
+            name: 'emailResponse'
+        }, {
+            type: 'list',
+            message: "What is the member's role?",
+            choices: ["Manager", "Engineer", "Intern"],
+            name: 'roleResponse',
+        }
+    ]).then(function ({
+        nameResponse,
+        idResponse,
+        emailResponse,
+        roleResponse
+    }) {
+        let question = ''
+        if (roleResponse == "Manager") {
+            question = "the manager phone number:";
+        } else if (roleResponse == "Engineer") {
+            question = "the engineer github username:";
+        } else if (roleResponse == "Intern") {
+            question = "the college the intern attended:";
+        }
+        inquirer.prompt([{
+            message: `Enter the ${question}`,
+            type: "input",
+            name: "memberInfo"
+        }]).then(function ({
+            memberInfo
+        }) {
+            if (roleResponse === "Manager") {
+                employee = new Manager(nameResponse, idResponse, emailResponse, memberInfo)
+                employees.push(employee);
+                role = "Manager";
+            } else if (roleResponse === "Engineer") {
+                employee = new Engineer(nameResponse, idResponse, emailResponse, memberInfo)
+                employees.push(employee);
+                role = "Engineer";
+            } else if (roleResponse === "Intern") {
+                employee = new Intern(nameResponse, idResponse, emailResponse, memberInfo)
+                employees.push(employee);
+                role = "Intern";
+            }
+            inquirer.prompt({
+                type: "list",
+                message: "Add another member?",
+                choices: ["Yes", "No"],
+                name: "response"
+            }).then(function ({
+                response
+            }) {
+                if (response === "Yes") {
+                    console.log(`${role} profile created`);
+                    CreateNewEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(employees), (err => err))
+                };
+            });
+        });
+    });
+};
---
+++ README.md
@@ -0,0 +1,52 @@
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://www.javascript.com/) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)   
+# Team Roster Generator
+
+## Description
+
+Takes user input from a series of prompts on CLI and generates a team roster webpage.
+
+## Table of Contents
+
+[1. Description](#Description)
+
+[2. Installation](#Installation)
+
+[3. Usage Information](#Usage)
+
+[4. License](#License)
+
+[5. Contributing](#Contributing)
+
+[6. Tests](#Tests)
+
+[7. Questions](#Questions)
+
+## Installation
+
+Before you run the program make sure to install the inquirer package or you will get a throw error.
+
+npm i inquirer
+
+## Usage
+
+You will be presented with a series of prompts regarding the team member information and assigned role. Your responses to these prompts will be inputted into the designated sections of the team roster. 
+
+## License
+
+Copyright (C) Anthony V. Billie Jr. this project is licensed under the [MIT License](/LICENSE.md).
+
+## Contributing
+
+Feel free to contribute to this repository, I only ask to keep your work as professional as possible.
+
+## Tests
+
+[Demo](https://youtu.be/xkRu3Y7zIXc)
+
+## Questions?
+
+Feel free to email all your questions to [Anthony V. Billie Jr.](mailto:anthonybilliejr2021@gmail.com?subject=[GitHub]%20Source%20).
+
+Want to see more? Visit my gitHub account [GitHub](https://github.com/avbillie)!
+            
